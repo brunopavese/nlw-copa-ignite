b394df7318315479a4a1700504d3d85e96f2a8b8
Bruno Pavese <bruno@ejcm.com.br>
2022-11-06 22:27:15 -0300

    feature: configurado o EsLint e Prettier no mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { NativeBaseProvider, StatusBar } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import React from "react";
 
-import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
-
+import { SignIn } from "./src/screens/SignIn";
 import { THEME } from "./src/styles/theme";
 
 export default function App() {
@@ -21,11 +20,7 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
